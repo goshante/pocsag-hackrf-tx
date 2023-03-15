032ee6b24944856e7e3da12e94e8db6fcffe1db9
Georgiy Malatsydze <heorhii.malatsydze@envisiontec.de>
2023-03-15 13:15:15 +0200

    Implemented queue

--- HackRF_Transmitter/FMModulator.h
+++ HackRF_Transmitter/FMModulator.h
@@ -8,12 +8,14 @@
 #include <atomic>
 #include <thread>
 #include <queue>
+#include <future>
 
 class FMModulator : public IHackRFData
 {
 private:
 	using WorkerBuf_t = std::vector<std::vector<int8_t>>;
-	using PCMQueue_t = std::queue<std::vector<float>>;
+	using PCMChunk_t = std::vector<float>;
+	using PCMQueue_t = std::queue<PCMChunk_t>;
 
 	HackRFDevice m_device;
 	std::mutex m_mutex;
@@ -28,19 +30,27 @@ private:
 	size_t m_sample_count;
 	uint32_t m_hackrf_sample;
 	float m_last_in_samples[4];
-	uint32_t m_chunkSizeSamples;
-	PCMQueue_t m_waveBuffer;
+	uint32_t m_subchunkSizeSamples;
+	PCMQueue_t m_waveQueue;
+	PCMChunk_t m_currentChunk;
 	WavSource::PCMHeader m_wavInfo;
-	size_t m_chunkOffset;
+	size_t m_subchunkOffset;
 	double m_FMdeviationKHz;
 	bool m_AM;
 	std::atomic<bool> m_ready;
 	double m_FM_phase;
+	std::thread* m_queueThread;
+	std::promise<bool> m_stopped;
+	std::promise<bool> m_started;
+	std::atomic<bool> m_stop;
+	std::atomic<bool> m_emptyQueue;
 
-	void interpolation();
-	void modulation();
-	void work(size_t offset);
-	void prepareNext();
+	void _interpolation();
+	void _modulation();
+	void _work(size_t offset);
+	bool _prepareNext();
+	void _workerThread();
+	void _nextSubChunk();
 
 protected:
 	int onData(int8_t* buffer, uint32_t length);
@@ -49,17 +59,15 @@ public:
 	FMModulator(float localGain);
 	~FMModulator();
 
-	void SetChunkSizeSamples(size_t count);
+	void SetSubChunkSizeSamples(size_t count);
 	void SetupFormat(WavSource::PCMHeader waveMetadata);
 	void PushSamples(const std::vector<float>& samples);
 	
-	void NextChunk();
+
 	uint32_t GetDeviceSampleRate();
 	uint32_t GetChunkSizeSamples();
 
-	bool Open();
-	void Close();
-	void SetFrequency(uint64_t freg);
+	void SetFrequency(uint64_t mhz, uint64_t khz, uint64_t hz = 0);
 	void SetGainRF(float gain);
 	void SetLocalGain(float gain);
 	void SetAMP(bool enableamp);
@@ -67,7 +75,7 @@ public:
 	bool StopTX();
 	void SetAM(bool set);
 	void SetFMDeviationKHz(double value);
-	bool ReadyNext();
+	bool IsIdle();
 };
 
 #endif
--- HackRF_Transmitter/main.cpp
+++ HackRF_Transmitter/main.cpp
@@ -8,34 +8,37 @@
 
 int main(int argc, char* argv[])
 {
-	POCSAG::Encoder pocsag(44100);
-	pocsag.SetAmplitude(8000);
-	std::vector<uint8_t> result;
-	pocsag.SetDateFormat(POCSAG::DateFormat::Begin);
-	pocsag.encode(result, 403361, POCSAG::Type::Alphanuberic, "Bibos Drulis!", POCSAG::BPS::BPS_512);
-
-	//WavSource* wav = new WavSource("D:\\NOLF2\\Game\\SND\\AMB\\RADIOCHATTER_1.WAV", SAMPLE_COUNT);
-	WavSource* wav = new WavSource("D:\\dsd\\test2.wav");
-	FMModulator* mod = new FMModulator(90);
-
-	if (!mod->Open())
-		return 0;
-
-	mod->SetupFormat(wav->pcmInfo());
-	mod->PushSamples(wav->getData());
-	mod->SetChunkSizeSamples(4096);
-	mod->SetFrequency(161'125'000);
-	mod->SetFMDeviationKHz(25.0e3);
-	mod->SetGainRF(40);
-	mod->SetAMP(true);
-	mod->StartTX();
-
-	while (((1 << 15) & GetAsyncKeyState(VK_ESCAPE)) == false)
+	try
 	{
-		Sleep(5);
-	}
+		POCSAG::Encoder pocsag;
+		pocsag.SetAmplitude(8000);
+		std::vector<uint8_t> result;
+		pocsag.SetDateFormat(POCSAG::DateFormat::Begin);
+		pocsag.encode(result, 403361, POCSAG::Type::Alphanuberic, "pRIWET vOPA", POCSAG::BPS::BPS_512);
+		WavSource wav(result);
+		FMModulator mod(90);
 
-	mod->StopTX();
-	delete(mod);
+		mod.SetupFormat(wav.pcmInfo());
+		mod.PushSamples(wav.getData());
+		mod.SetSubChunkSizeSamples(4096);
+		mod.SetFrequency(161, 125);
+		mod.SetFMDeviationKHz(25.0);
+		mod.SetGainRF(40);
+		mod.SetAMP(true);
+		mod.StartTX();
+
+		while (((1 << 15) & GetAsyncKeyState(VK_ESCAPE)) == false && mod.IsIdle())
+		{
+			Sleep(5);
+		}
+
+		mod.StopTX();
+	}
+	catch (const std::exception& ex)
+	{
+		std::cout << "Error: " << ex.what() << std::endl;
+	}
+	
+	system("pause");
 	return 0;
 }
